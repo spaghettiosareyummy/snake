1b6b6055dd27d4eb6cf2dcaf69c97c819dd3706b
spaghetti <bmbowdoin03@gmail.com>
2022-01-21 20:31:27 -0600

    better death

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pygame.math import Vector2 #allows us to just type "Vector2" instead of pyg
 
 class SNAKE:
     def __init__(self):
-        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+        self.body = [Vector2(5,10),Vector2(4,0),Vector2(3,10)]
         self.direction = Vector2(1,0)
         self.new_block = False
 
@@ -65,8 +65,12 @@ class MAIN:
             self.snake.add_block()
 
     def check_death(self):
-        if not 0 <= self.snake.body[0].x <= cell_number:
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
             self.game_over()
+
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
 
     def game_over(self):
         pygame.quit()
@@ -93,13 +97,17 @@ while True:
             main_game.update()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                main_game.snake.direction = Vector2(0,-1)
+                if main_game.snake.direction.y != 1:
+                    main_game.snake.direction = Vector2(0,-1)
             if event.key == pygame.K_DOWN:
-                main_game.snake.direction = Vector2(0,1)
+                if main_game.snake.direction.y != -1:
+                    main_game.snake.direction = Vector2(0,1)
             if event.key == pygame.K_LEFT:
-                main_game.snake.direction = Vector2(-1,0)
+                if main_game.snake.direction.x != 1:
+                    main_game.snake.direction = Vector2(-1,0)
             if event.key == pygame.K_RIGHT:
-                main_game.snake.direction = Vector2(1,0)
+                if main_game.snake.direction.x != -1:
+                    main_game.snake.direction = Vector2(1,0)
 
     screen.fill((175,215,70))
     main_game.draw_elements()
