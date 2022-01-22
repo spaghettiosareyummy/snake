bad22564a881576555646d18ec6d1c61b205bc75
spaghetti <bmbowdoin03@gmail.com>
2022-01-21 19:50:19 -0600

    added keyboard inputs

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame, sys,random #imports pygame and sys :)
 from pygame.math import Vector2 #allows us to just type "Vector2" instead of pygame.math.Vector2()
 
+
+
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+        self.direction = Vector2(1,0)
 
     def draw_snake(self):
         for block in self.body:
@@ -11,6 +14,11 @@ class SNAKE:
             y_pos = int(block.y * cell_size)
             block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
             pygame.draw.rect(screen,(183,111,122),block_rect)
+    
+    def move_snake(self):
+        body_copy = self.body[:-1] #haha looks like pp :troll:
+        body_copy.insert(0,body_copy[0] + self.direction)
+        self.body = body_copy[:]
 
 class FRUIT:
     def __init__(self):
@@ -31,11 +39,26 @@ clock = pygame.time.Clock() #helps us influence the time in pygame
 fruit = FRUIT()
 snake = SNAKE()
 
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150)
+
 while True:
     for event in pygame.event.get(): #checking for every possible event
         if event.type == pygame.QUIT: #checks if you press the X
             pygame.quit #quits the game
             sys.exit() #ends any code that is being run
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.direction = Vector2(0,-1)
+            if event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0,1)
+            if event.key == pygame.K_LEFT:
+                snake.direction = Vector2(-1,0)
+            if event.key == pygame.K_RIGHT:
+                snake.direction = Vector2(1,0)
+
     screen.fill((175,215,70))
     fruit.draw_fruit()
     snake.draw_snake()
