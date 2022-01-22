204e2cd568051bfef30c4e85405e1de6b5c792ed
spaghetti <bmbowdoin03@gmail.com>
2022-01-22 10:40:47 -0600

    pain

--- main.py
+++ main.py
@@ -25,22 +25,10 @@ class SNAKE:
         self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
         self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
 
-    def update_head_graphics(self):
-        head_relation = self.body[1] - self.body[0]
-        if head_relation == Vector2(1,0): self.head = self.head_left
-        elif head_relation == Vector2(-1,0): self.head = self.head_right
-        elif head_relation == Vector2(0,1): self.head = self.head_up
-        elif head_relation == Vector2(0,-1): self.head = self.head_down
 
-    def update_tail_graphics(self):
-        tail_relation = self.body[-2] - self.body[-1]
-        if tail_relation == Vector2(1,0): self.tail = self.tail_left
-        elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
-        elif tail_relation == Vector2(0,1): self.tail = self.tail_up
-        elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
 
     def draw_snake(self):
-        self.update_head_graphics()
+        self.update_headgraphics()
         self.update_tail_graphics()
 
         for index,block in enumerate(self.body):
@@ -69,7 +57,23 @@ class SNAKE:
                     elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
                         screen.blit(self.body_br,block_rect)
 
-    
+    def update_headgraphics(self):
+        head_dir = self.body[1] - self.body[0]
+        if head_dir == Vector2(1,0): 
+            self.head = self.head_left
+        elif head_dir == Vector2(-1,0): 
+            self.head = self.head_right
+        elif head_dir == Vector2(0,1): 
+            self.head = self.head_up
+        elif head_dir == Vector2(0,-1): 
+            self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1,0): self.tail = self.tail_left
+        elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
+        elif tail_relation == Vector2(0,1): self.tail = self.tail_up
+        elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
 
     def move_snake(self):
         if self.new_block == True:
