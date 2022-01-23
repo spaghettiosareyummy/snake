d845344d863d1ffea2920f9906e951eff1d70957
spaghetti <bmbowdoin03@gmail.com>
2022-01-22 20:33:51 -0600

    Somewhat decent escape menu

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ class OptButton:
             else:
                 self.dyel = self.elevation
                 if self.pressed == True:
-                    self.nothing.play()
+                    # self.nothing.play()
                     self.pressed = False
                     opt_menu()
         else:
@@ -180,9 +180,58 @@ class BackButton:
             else:
                 self.dyel = self.elevation
                 if self.pressed == True:
-                    self.nothing.play()
+                    # self.nothing.play()
                     self.pressed = False
                     start_menu()
+        else:
+            self.dyel = self.elevation
+            self.top_color = '#31373e'
+
+class ResetButton:
+    def __init__(self,text,width,height,pos,elevation):
+        self.pressed = False
+        self.elevation = elevation
+        self.dyel = elevation
+        self.origin_y = pos[1]
+
+        self.top_rect = pygame.Rect(pos,(width,height))
+        self.top_color = '#31373e'
+
+        self.bottom_rect = pygame.Rect(pos,(width,elevation))
+        self.bottom_color = '#231f20'
+
+        self.text_surf = game_font.render(text,True,'#FFFFFF')
+        self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
+        self.snake = SNAKE()
+        
+        # self.click_sound = pygame.mixer.Sound(r'Sounds\mouse-click.mp3')
+
+    def draw(self):
+        self.top_rect.y = self.origin_y - self.dyel
+        self.text_rect.center = self.top_rect.center
+
+        self.bottom_rect.midtop = self.top_rect.midtop
+        self.bottom_rect.height = self.top_rect.height + self.dyel
+
+        pygame.draw.rect(screen,self.bottom_color,self.bottom_rect,border_radius=12)
+        pygame.draw.rect(screen,self.top_color,self.top_rect,border_radius = 12)
+        screen.blit(self.text_surf,self.text_rect)
+        self.click()
+
+    def click(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.top_rect.collidepoint(mouse_pos):
+            self.top_color = '#7289da'
+            if pygame.mouse.get_pressed()[0]:
+                self.dyel = 0
+                self.pressed = True
+            else:
+                self.dyel = self.elevation
+                if self.pressed == True:
+                    # self.nothing.play()
+                    self.pressed = False
+                    main_play()
+                    self.snake.reset()
         else:
             self.dyel = self.elevation
             self.top_color = '#31373e'
@@ -287,6 +336,10 @@ class SNAKE:
     def reset(self):
         self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
         self.direction = Vector2(0,0)
+    # def escreset(self):
+    #     self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
+    #     self.direction = Vector2(0,0)
+        
 
 class FRUIT:
     def __init__(self):
@@ -398,13 +451,18 @@ screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_si
 mainClock = pygame.time.Clock() #helps me influence the time in pygame
 startClock = pygame.time.Clock()
 credClock = pygame.time.Clock()
+escClock = pygame.time.Clock()
 apple = pygame.image.load(r'Graphics\apple.png').convert_alpha()
 game_font = pygame.font.Font(r'Fonts\PoetsenOne-Regular.ttf',25)
+back_arrow = pygame.image.load(r'Graphics\OIP.jfif')
+esc_font = pygame.font.Font(r'Fonts\PoetsenOne-Regular.ttf',15)
 
 buttonstart = StartButton('Start Game',200,40,(200,200),6)
 buttonop = OptButton('Options', 200, 40, (200,250),6)
 buttoncred = CredButton('Credits',200,40,(200,300),6)
 buttonback = BackButton('Return to Menu',200,40,(20,550),6)
+buttonbackesc = BackButton('Return to Menu',200,40,(195,300),6)
+buttonreset = ResetButton('Reset',200,40,(195,350),6)
 
 
 SCREEN_UPDATE = pygame.USEREVENT
@@ -437,7 +495,7 @@ def cred_menu():
                 pygame.quit
                 sys.exit()
         text = game_font.render('Made by spaghetti =)',True,'#FFFFFF')
-        screen.blit(text, (177,200))
+        screen.blit(text, (300,400))
 
         buttonback.draw()
 
@@ -454,12 +512,34 @@ def opt_menu():
         text = game_font.render('Trolled e.e',True,'#FFFFFF')
         screen.blit(text, (177,200))
 
+        buttonback.draw()
+
         pygame.display.update()
         credClock.tick(60)
+
+def esc_menu():
+    while True:
+        screen.fill((175,215,70))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    main_play()
+        
+        buttonbackesc.draw()
+        buttonreset.draw()
+        
+        pygame.display.update()
+        escClock.tick(60)
+        
+
 
 
 def main_play():
     while True:
+        tutorial = True
         for event in pygame.event.get(): #checking for every possible event
             if event.type == pygame.QUIT: #checks if you press the X
                 pygame.quit #quits the game
@@ -468,21 +548,27 @@ def main_play():
                 main_game.update()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
+                    tutorial = False
                     if main_game.snake.direction.y != 1:
                         main_game.snake.direction = Vector2(0,-1)
                 if event.key == pygame.K_DOWN:
+                    tutorial = False
                     if main_game.snake.direction.y != -1:
                         main_game.snake.direction = Vector2(0,1)
                 if event.key == pygame.K_LEFT:
+                    tutorial = False
                     if main_game.snake.direction.x != 1:
                         main_game.snake.direction = Vector2(-1,0)
                 if event.key == pygame.K_RIGHT:
+                    tutorial = False
                     if main_game.snake.direction.x != -1:
                         main_game.snake.direction = Vector2(1,0)
+                if event.key == pygame.K_ESCAPE:
+                    esc_menu()
 
         screen.fill((175,215,70))
         main_game.draw_elements()
         pygame.display.update() #displays all of the new info on the display surface
         mainClock.tick(60) #runs at 60 FPS
 
-cred_menu()
+esc_menu()
